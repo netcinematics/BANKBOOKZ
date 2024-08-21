2ef37e75d3f366d496af28a93e4ebc454434cc1e
NetCinematics <alpinefalcon@protonmail.com>
2024-08-21 15:53:57 -0700

    spot

--- module/TRADESPOT_1.js
+++ module/TRADESPOT_1.js
@@ -1,124 +1,128 @@
-var _ = Object.defineProperty;
-var u = (s,t,e)=>t in s ? _(s, t, {enumerable: !0,configurable: !0,writable: !0, value: e}) : s[t] = e;
-var i = (s,t,e)=>(u(s, typeof t != "symbol" ? t + "" : t, e),e);
-// import {V as p} from "./lightweight-charts.production-7ee7078d.js";
-// import {g as w} from "./sample-data-c7d89ab8.js";
-// import {p as m} from "./positions-0a54307c.js";
-// import {P as lwcprod} from "./lwc.prod.js";
-// import {g as w} from "./sample-data.js";
-import {P as plugBase} from "./plugin-base.js";
-import {p as m} from "./positions.js";
-
-class VLineDraw_Class {
-    constructor(t, e) {
-        i(this, "_x", null);
-        i(this, "_options");
-        this._x = t,
-        this._options = e
+var objprops = Object.defineProperty;
+var typeset = (a,t,i)=>t in a ? objprops(a, t, {
+    enumerable: !0, configurable: !0, writable: !0, value: i}) : a[t] = i;
+var makeMember = (a,t,i)=>(typeset(a, typeof t != "symbol" ? t + "" : t, i),i);
+import {P as lwcprod} from "./lwc.prod.js";
+import {g as getRandomDataSet} from "./sample-data.js";
+class PointDraw_Class {
+    constructor(p1, p2, txt1, txt2, opts) {
+        makeMember(this, "_p1");
+        makeMember(this, "_p2");
+        makeMember(this, "_text1");
+        makeMember(this, "_text2");
+        makeMember(this, "_options");
+        this._p1 = p1,
+        this._p2 = p2,
+        this._text1 = txt1,
+        this._text2 = txt2,
+        this._options = opts
     }
-    draw(t) {
-        t.useBitmapCoordinateSpace(e=>{
-            if (this._x === null)
+    draw(parent) {
+        parent.useBitmapCoordinateSpace(i=>{
+            if (this._p1.x === null || this._p1.y === null || this._p2.x === null || this._p2.y === null)
                 return;
-            const n = e.context
-              , a = m(this._x, e.horizontalPixelRatio, this._options.width);
-            n.fillStyle = this._options.color,
-            n.fillRect(a.position, 0, a.length, e.bitmapSize.height)
-        }
-        )
+            const ctx = i.context
+            let p1x = Math.round(this._p1.x * i.horizontalPixelRatio)
+            let p1y = Math.round(this._p1.y * i.verticalPixelRatio)
+            let p2x = Math.round(this._p2.x * i.horizontalPixelRatio)
+            let p2y = Math.round(this._p2.y * i.verticalPixelRatio);
+            ctx.lineWidth = this._options.width;
+            ctx.strokeStyle = this._options.lineColor;
+            // ctx.lineDashOffset = 0;
+            ctx.beginPath();
+            // ctx.setLineDash([6, 8])
+            ctx.lineCap = "round";
+            ctx.moveTo(p1x, p1y);        //DRAW-PLUS
+            ctx.lineTo(p1x-10, p1y);
+            ctx.moveTo(p1x-5, p1y+10);        //DRAW-PLUS
+            ctx.lineTo(p1x-5, p1y-10);
+            ctx.moveTo(p2x+10, p2y+10);        //DRAW-X
+            ctx.lineTo(p2x-10, p2y-10);
+            ctx.moveTo(p2x+10, p2y-10);
+            ctx.lineTo(p2x-10, p2y+10);
+            // ctx.lineTo(p2x, p2y);
+            ctx.stroke();
+            // this._options.showLabels && (this._drawTextLabel(i, this._text1, p1x, p1y, !0),
+            // this._drawTextLabel(i, this._text2, p2x, p2y, !1))
+        })
+    }
+    _drawTextLabel(t, i, e, o, s) {
+        t.context.font = "12px Verdana",
+        t.context.beginPath();
+        const r = 5 * t.horizontalPixelRatio
+        let h = t.context.measureText(i)
+        let p = s ? h.width + r * 4 : 0;
+        t.context.fillStyle = this._options.labelBackgroundColor,
+        // t.context.roundRect(e + r - p, o - 24, h.width + r * 2, 24 + r, 5),
+        t.context.roundRect(e + r - p, o-15, h.width + r * 2, 20, 5),
+        t.context.fill(),
+        t.context.beginPath(),
+        t.context.fillStyle = this._options.labelTextColor,
+        t.context.fillText(i, e + r * 2 - p, o)
     }
 }
-class VLineRender_Class {
-    constructor(t, e) {
-        i(this, "_source");
-        i(this, "_x", null);
-        i(this, "_options");
-        this._source = t,
-        this._options = e
+class PointRender_Class {
+    constructor(parent) {
+        makeMember(this, "_source");
+        makeMember(this, "_p1", { x: null,  y: null });
+        makeMember(this, "_p2", { x: null,  y: null });
+        this._source = parent
     }
     update() {
-        const t = this._source._chart.timeScale();
-        this._x = t.timeToCoordinate(this._source._time)
+        const t = this._source._series
+        let i = t.priceToCoordinate(this._source._p1.price)
+        let e = t.priceToCoordinate(this._source._p2.price)
+        let o = this._source._chart.timeScale()
+        let s = o.timeToCoordinate(this._source._p1.time)
+        let r = o.timeToCoordinate(this._source._p2.time);
+        // this._p1 = { x: s,  y: s+10 },
+        // this._p2 = { x: r,  y: r+10  }
+        this._p1 = { x: s,  y: i  },
+        this._p2 = { x: r,  y: e  }
     }
     renderer() {
-        return new VLineDraw_Class(this._x,this._options)
+        return new PointDraw_Class(this._p1,this._p2,"" + this._source._p1.price.toFixed(1),"" + this._source._p2.price.toFixed(1),this._source._options)
     }
 }
-class VLinePane_Class {// extends plugBase {
-    constructor(t, e) {
-        // super();
-        i(this, "_source");
-        i(this, "_x", null);
-        i(this, "_options");
-        this._source = t,
-        this._options = e
-    }
-    update() {
-        const t = this._source._chart.timeScale();
-        this._x = t.timeToCoordinate(this._source._time)
-    }
-    visible() {
-        return this._options.showLabel
-    }
-    tickVisible() {
-        return this._options.showLabel
-    }
-    coordinate() {
-        return this._x ?? 0
-    }
-    text() {
-        return this._options.labelText
-    }
-    textColor() {
-        return this._options.labelTextColor
-    }
-    backColor() {
-        return this._options.labelBackgroundColor
-    }
-}
-const vline_Default_Options = {
-    color: "green",labelText: "",width: 3,labelBackgroundColor: "green",labelTextColor: "white",
-    showLabel: !1
+const PointOptions = {width:2,showLabels:true,lineColor:"rgb(255, 69, 44)", //AVG ORANGE
+    labelBackgroundColor: "rgba(0, 0, 55, 0.85)", labelTextColor: "steelblue"
 };
-class VLineOverlay_Class {
-    constructor(t, e, n, a) {
-        i(this, "_chart");
-        i(this, "_series");
-        i(this, "_time");
-        i(this, "_paneViews");
-        i(this, "_timeAxisViews");
-        const h = {
-            ...vline_Default_Options,
-            ...a
-        };
-        this._chart = t,
-        this._series = e,
-        this._time = n,
-        this._paneViews = [new VLineRender_Class(this,h)],
-        this._timeAxisViews = [new VLinePane_Class(this,h)]
+class TradeSpot_Class { //chartElem,lwc,EndPoint,StartPoint
+    constructor(elem, lwc, strtpt, endpt,  opts) {
+        makeMember(this, "_chart");
+        makeMember(this, "_series");
+        makeMember(this, "_p1");
+        makeMember(this, "_p2");
+        makeMember(this, "_paneViews");
+        makeMember(this, "_options");
+        makeMember(this, "_minPrice");
+        makeMember(this, "_maxPrice");
+        this._chart = elem,
+        this._series = lwc,
+        this._p1 = strtpt,
+        this._p2 = endpt,
+        this._minPrice = Math.min(this._p1.price, this._p2.price),
+        this._maxPrice = Math.max(this._p1.price, this._p2.price),
+        this._options = { ...PointOptions, ...opts },
+        this._paneViews = [new PointRender_Class(this)]
+    }
+    autoscaleInfo(t, i) {
+        const e = this._pointIndex(this._p1)
+        let o = this._pointIndex(this._p2);
+        return e === null || o === null || i < e || t > o ? null : {
+            priceRange: { minValue: this._minPrice, maxValue: this._maxPrice }
+        }
     }
     updateAllViews() {
-        this._paneViews.forEach(t=>t.update()),
-        this._timeAxisViews.forEach(t=>t.update())
-    }
-    timeAxisViews() {
-        return this._timeAxisViews
+        this._paneViews.forEach(t=>t.update())
     }
     paneViews() {
         return this._paneViews
     }
+    _pointIndex(t) {
+        const i = this._chart.timeScale().timeToCoordinate(t.time);
+        return i === null ? null : this._chart.timeScale().coordinateToLogical(i)
+    }
 }
 
-export {VLineOverlay_Class as VLineOverlay_Class};
-
-// const l = lwcprod("chart", {autoSize: !0})
-// const r = l.addLineSeries()
-// const o = w();
-// r.setData(o);
-// const L = new VLineOverlay_Class(l,r,o[o.length - 50].time,{
-//     showLabel: !0, labelText: "1"});
-// r.attachPrimitive(L);
-// const f = new VLineOverlay_Class(l,r,o[o.length - 25].time,{
-//     showLabel: !1,color: "red",width: 2
-// });
-// r.attachPrimitive(f);
+export {TradeSpot_Class as TradeSpot_Class};
--- module/TRADESPOT_3.js
+++ module/TRADESPOT_3.js
@@ -5,45 +5,66 @@ var makeMember = (a,t,i)=>(typeset(a, typeof t != "symbol" ? t + "" : t, i),i);
 import {P as lwcprod} from "./lwc.prod.js";
 import {g as getRandomDataSet} from "./sample-data.js";
 class PointDraw_Class {
-    constructor(p1, p2, txt1, txt2, opts) {
+    constructor(p1, type, txt1, opts) {
         makeMember(this, "_p1");
-        makeMember(this, "_p2");
+        makeMember(this, "_type");
         makeMember(this, "_text1");
-        makeMember(this, "_text2");
+        // makeMember(this, "_text2");
         makeMember(this, "_options");
         this._p1 = p1,
-        this._p2 = p2,
+        this._type = type,
         this._text1 = txt1,
-        this._text2 = txt2,
+        // this._text2 = txt2,
         this._options = opts
     }
     draw(parent) {
         parent.useBitmapCoordinateSpace(i=>{
-            if (this._p1.x === null || this._p1.y === null || this._p2.x === null || this._p2.y === null)
-                return;
+            // if (this._p1.x === null || this._p1.y === null || this._p2.x === null || this._p2.y === null)
+            if (this._p1.x === null || this._p1.y === null){ return; }
             const ctx = i.context
             let p1x = Math.round(this._p1.x * i.horizontalPixelRatio)
             let p1y = Math.round(this._p1.y * i.verticalPixelRatio)
-            let p2x = Math.round(this._p2.x * i.horizontalPixelRatio)
-            let p2y = Math.round(this._p2.y * i.verticalPixelRatio);
+            // let p2x = Math.round(this._p2.x * i.horizontalPixelRatio)
+            // let p2y = Math.round(this._p2.y * i.verticalPixelRatio);
             ctx.lineWidth = this._options.width;
             ctx.strokeStyle = this._options.lineColor;
             // ctx.lineDashOffset = 0;
-            ctx.beginPath();
+            // debugger;
+            
             // ctx.setLineDash([6, 8])
-            ctx.lineCap = "round";
-            //----------------------------//DRAW-PLUS sign
-            ctx.moveTo(p1x, p1y);        
-            ctx.lineTo(p1x-6, p1y);
-            ctx.moveTo(p1x-3, p1y+3);        
-            ctx.lineTo(p1x-3, p1y-3);
-            //------------------------------   //DRAW-X
-            ctx.moveTo(p2x+10, p2y+10);     
-            ctx.lineTo(p2x-10, p2y-10);
-            ctx.moveTo(p2x+10, p2y-10);
-            ctx.lineTo(p2x-10, p2y+10);
-            // ctx.lineTo(p2x, p2y);
-            ctx.stroke();
+            // ctx.strokeStyle = "rgb(88, 1, 144)"; //Deeppurple
+            if(this._type==='+'){  //sml, big, lrg
+                //----------------------------//DRAW-PLUS sign
+                ctx.beginPath();
+                ctx.lineCap = "round";
+                ctx.moveTo(p1x+4, p1y);        
+                ctx.lineTo(p1x-4, p1y);
+                ctx.moveTo(p1x, p1y+4);        
+                ctx.lineTo(p1x, p1y-4);
+                ctx.stroke();
+            }
+            else if (this._type==='X'){
+                //------------------------------   //DRAW-X
+                ctx.beginPath();
+                ctx.strokeStyle = this._options.lineColor;
+                ctx.moveTo(p1x+4, p1y+4);     
+                ctx.lineTo(p1x-4, p1y-4);
+                ctx.moveTo(p1x+4, p1y-4);
+                ctx.lineTo(p1x-4, p1y+4);
+                ctx.stroke();
+            }
+            else{
+                console.log('no type found')
+                //------------------------------   //DRAW-SQUARE
+                ctx.beginPath();
+                ctx.strokeStyle = this._options.lineColor;
+                ctx.moveTo(p1x+4, p1y+4);     
+                ctx.lineTo(p1x+4, p1y-4);
+                ctx.lineTo(p1x-4, p1y-4);
+                ctx.lineTo(p1x-4, p1y+4);
+                ctx.lineTo(p1x+4, p1y+4);
+                ctx.stroke();                
+            }
             // this._options.showLabels && (this._drawTextLabel(i, this._text1, p1x, p1y, !0),
             // this._drawTextLabel(i, this._text2, p2x, p2y, !1))
         })
@@ -67,32 +88,35 @@ class PointRender_Class {
     constructor(parent) {
         makeMember(this, "_source");
         makeMember(this, "_p1", { x: null,  y: null });
-        makeMember(this, "_p2", { x: null,  y: null });
+        makeMember(this, "_type","");
         this._source = parent
     }
     update() {
         const aSeries = this._source._series
         let yPT1 = aSeries.priceToCoordinate(this._source._p1.price)
-        let yPT2 = aSeries.priceToCoordinate(this._source._p2.price)
+        // let yPT2 = aSeries.priceToCoordinate(this._source._p2.price)
         let timeScale = this._source._chart.timeScale()
         let xPT1 = timeScale.timeToCoordinate(this._source._p1.time)
-        let xPT2 = timeScale.timeToCoordinate(this._source._p2.time);
-        this._p1 = { x: xPT1,  y: yPT1  },
-        this._p2 = { x: xPT2,  y: yPT2  }
+        // let xPT2 = timeScale.timeToCoordinate(this._source._p2.time);
+        this._p1 = { x: xPT1,  y: yPT1  };
+
+        this._type = this._source._type;
+        // this._p2 = { x: xPT2,  y: yPT2  }
     }
     renderer() {
-        return new PointDraw_Class(this._p1,this._p2,"" + this._source._p1.price.toFixed(1),"" + this._source._p2.price.toFixed(1),this._source._options)
+        return new PointDraw_Class(this._p1,this._type,"" + this._source._p1.price.toFixed(1),this._source._options)
+        // return new PointDraw_Class(this._p1,this._p2,"" + this._source._p1.price.toFixed(1),"" + this._source._p2.price.toFixed(1),this._source._options)
     }
 }
 const PointOptions = {width:2,showLabels:true,lineColor:'steelblue', //
     labelBackgroundColor: "rgba(0, 0, 55, 0.85)", labelTextColor: "steelblue"
 };
 class TradeSpot_Class { //chartElem,lwc,EndPoint,StartPoint
-    constructor(elem, lwc, strtpt, endpt,  opts) {
+    constructor(elem, lwc, strtpt, type,  opts) {
         makeMember(this, "_chart");
         makeMember(this, "_series");
         makeMember(this, "_p1");
-        makeMember(this, "_p2");
+        makeMember(this, "_type");
         makeMember(this, "_paneViews");
         makeMember(this, "_options");
         makeMember(this, "_minPrice");
@@ -100,16 +124,19 @@ class TradeSpot_Class { //chartElem,lwc,EndPoint,StartPoint
         this._chart = elem,
         this._series = lwc,
         this._p1 = strtpt,
-        this._p2 = endpt,
-        this._minPrice = Math.min(this._p1.price, this._p2.price),
-        this._maxPrice = Math.max(this._p1.price, this._p2.price),
+        this._type = type,
+        this._minPrice = Math.min(this._p1.price),
+        this._maxPrice = Math.max(this._p1.price),
+        // this._minPrice = Math.min(this._p1.price, this._p2.price),
+        // this._maxPrice = Math.max(this._p1.price, this._p2.price),
         this._options = { ...PointOptions, ...opts },
         this._paneViews = [new PointRender_Class(this)]
     }
     autoscaleInfo(t, i) {
-        const e = this._pointIndex(this._p1)
-        let o = this._pointIndex(this._p2);
-        return e === null || o === null || i < e || t > o ? null : {
+        const P1 = this._pointIndex(this._p1)
+        // let o = this._pointIndex(this._p2);
+        // return P1 === null || o === null || i < P1 || t > o ? null : {
+        return P1 === null || i < P1 || P1 ? null : {
             priceRange: { minValue: this._minPrice, maxValue: this._maxPrice }
         }
     }
