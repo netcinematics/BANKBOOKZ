2505044aa48238c98abcfeee899ee24540654325
NetCinematics <alpinefalcon@protonmail.com>
2024-09-25 14:13:51 -0700

    algos

--- module/ALGORITHMZ_4.js
+++ module/ALGORITHMZ_4.js
@@ -16,9 +16,23 @@ function calcMA(candleData, maLength) {
     return maData;
 } 
 
-function calcSELLZ(){return 1}
-function calcBREV(){return 2}
-function calcBUYZ(){return 3}
+function calcBuySellAVG_Lines(trades){
+    debugger;
+    // const buySellBrev = {buy:0,sell:0,brev:0};
+    let buyAMT=0,buyCNT=0,buyAVG=0,sellAMT=0,sellCNT=0,sellAVG=0,brev=0;
+    for(let trade of trades){
+        if(trade.type==='BUY'){ buyCNT++;
+            buyAMT += trade.price;
+        } else if(trade.type==='SELL'){ sellCNT++;
+            sellAMT += trade.price;
+        }
+    }
+    if(sellAMT && sellCNT){ sellAVG = Math.round(sellAMT/sellCNT)}
+    if(buyAMT && buyCNT){ buyAVG = Math.round(buyAMT/buyCNT)}
+    if(buyAVG && sellAVG){ brev = Math.round(buyAVG+sellAVG/2)}
+    return {buy:buyAVG,sell:sellAVG,brev:brev};
 
-export {calcMA,calcSELLZ,calcBUYZ,calcBREV};
+}
+
+export {calcMA,calcBuySellAVG_Lines};
 
